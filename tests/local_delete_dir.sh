f6ab2a36b1da3240734c78affd52e3781af2e934
Kasper Markus <kasper2@markus.dk>
2009-12-31 12:46:05 +0100

    added a series of test cases, to check out the sync functionality. The test cases are by no means thorough and sufficient. They test the simplest of cases and should be extended at a later point

---
+++ tests/local_delete_dir.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#sets up a test case for repo_sync and repo_watch
+if [ -f ~/.grimreporc ]; then
+	source ~/.grimreporc
+else 
+	source `pwd`/.grimreporc
+fi
+function do_test {
+	#make two identical dirs
+	rm -rf "${GR_LOCALROOT}" "$GR_SERVERROOT"
+	mkdir -p "${GR_LOCALROOT}"{A,B,C}
+	touch "${GR_LOCALROOT}"{A/a1,B/b1,B/b2,C/c1}
+	cp -r "${GR_LOCALROOT}" "${GR_SERVERROOT}"
+
+	#initialize repository
+	./repo_init.sh
+
+	#delete dir from local
+	rm -rf "${GR_LOCALROOT}"C
+	#make directory locally
+	./repo_sync.sh
+}
