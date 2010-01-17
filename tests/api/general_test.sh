9aac06581ad973e1f06600aae780f804cbed2fff
kij <kijmail@gmail.com>
2010-01-17 18:13:11 +0000

    api general_test.sh

--- tests/api/general_test.sh
+++ tests/api/general_test.sh
@@ -1,58 +1,42 @@
-#!/bin/bash
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  general_test.sh
+# 
+#         USAGE:  ./general_test.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Dr. Fritz Mehner (fgm), mehner@fh-swf.de
+#       COMPANY: FH Südwestfalen, Iserlohn
+#       CREATED: 01/17/2010 05:52:29 PM UTC
+#      REVISION:  ---
+#===============================================================================
 
-	source `pwd`/.grimreporc
+set -o nounset                              # Treat unset variables as an error
 
-
-
-
-#sync dirs
-	rm -rf "${GR_LOCALROOT}" "$GR_SERVERROOT"
-	mkdir -p "${GR_LOCALROOT}"{A,B,C}
-	touch "${GR_LOCALROOT}"{A/a1,B/b1,B/b2,C/c1}
-	cp -r "${GR_LOCALROOT}" "${GR_SERVERROOT}"
-
-
-
-#initialize repository
-./repo_init.sh
-#number of tests
-TESTS=0;
-#number of good tests
-SUCCESS=0;
-
-echo testing:
-
-#1expect no output
-let TESTS=TESTS+1
+function 1test_1() {
 out=`./repo_sync.sh 2> /dev/null`
-if [[ ! "$out"=="" ]]; then
-	echo 'should be empty';
-else
-    let SUCCESS=SUCCESS+1
-fi
+assert_equal $out "" "Testing default dont have changes"
+}
 
 
-let TESTS=TESTS+1
+function 1test_2() {
 echo "xxx" > ${GR_LOCALROOT}A/a1
 echo "bbb" > ${GR_SERVERROOT}A/a1
 
-cat ${GR_LOCALROOT}A/a1
-cat ${GR_SERVERROOT}A/a1
-test_out=`./repo_sync.sh`
-
+test_out=`./repo_sync.sh 2>/dev/null`
+assert_equal $out ""
 test_goal="1A/a1
 FILE_CHANGED_BOTH
 d41d8cd98f00b204e9800998ecf8427e
 b8694d27c0f13f22ed3bc610c19ec15"
 
+assert_equal "$test_out" "$test_goal" "Testing two local file changes" 
+}
 
-if [[ "$test_out" == "$test_goal" ]]; then
-	echo ok
-    let SUCCESS=SUCCESS+1
-fi
-
-
-echo "######results######"
-echo "total tests:   $TESTS"
-echo "successes  :   $SUCCESS"
 
